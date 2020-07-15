e653c0c8240332c404ee444072e8fb2c80de366b
Sayan <mstu1@yandex.ru>
2020-07-15 18:35:34 +0300

    first commit

---
+++ src/main/java/application/ServerApp.java
@@ -0,0 +1,14 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ServerApp {
+
+    public static void main(String[] args){
+        SpringApplication.run(ServerApp.class, args);
+    }
+}
